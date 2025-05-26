aad9e006536e87af3e4102f3e688ba701cc41918
George <gcharris@calpoly.edu>
2025-05-25 23:17:00 -0700

    Added Initial Updates to Website

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,9 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Aircraft from './pages/Aircraft';
+import Team from './pages/Team';
+import Careers from './pages/Careers';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 function App() {
-    return (
-        <div>
-            <h1>My cool webpage</h1>
-        </div>
-    );
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aircraft" element={<Aircraft />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/careers" element={<Careers />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
